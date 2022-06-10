a052039d7feb867dd9c41353fcd0add18a3e1efb
Nastasia <Nastasia199126@gmail.com>
2022-06-10 12:27:39 +0400

    add chenges

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return (
-    <h1 className='gradient'>
-      Home page
-    </h1>
-  )
+
+  return <h1 className='gradient'>
+    Home page
+  </h1>
 }
 
 export default Home
